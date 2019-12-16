727a47d347554b8ce179b56db612c08426a881fb
Tatyana Fomina <tayna-anita@mail.ru>
2019-12-16 19:41:02 +0300

    Базовые настройки вебпака

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.jsx",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+    publicPath: "/"
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "src"),
+    compress: true,
+    port: 3000,
+    hot: true,
+    historyApiFallback: true,
+    watchContentBase: true,
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+      title: "Deutsch Daily Question"
+    })
+  ]
+};
